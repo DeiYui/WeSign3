75a2620d1837ab7cd90947d745425f4bc84db244
truongnv <truongnv@vnpt-technology.vn>
2024-06-09 20:25:34 +0700

    Cập nhật dữ liệu chung , riêng

--- src/hooks/useContext.tsx
+++ src/hooks/useContext.tsx
@@ -36,29 +36,16 @@ const SocketContext = createContext<SocketContextProps | undefined>(undefined);
 
 const peerConnectionConfig = {
   iceServers: [
-    { urls: "stun:freeturn.net:5349" },
-    {
-      urls: "turns:freeturn.tel:5349",
-    },
-    {
-      urls: "relay1.expressturn.com:3478",
-    },
-    { urls: "stun:stun.relay.metered.ca:80" },
-    {
-      urls: "turn:asia-east.relay.metered.ca:80",
-    },
-    {
-      urls: "turn:asia-east.relay.metered.ca:80?transport=tcp",
-    },
-    {
-      urls: "turn:asia-east.relay.metered.ca:443",
-    },
-    {
-      urls: "turns:asia-east.relay.metered.ca:443?transport=tcp",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:80",
-    },
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun.l.google.com:5349" },
+    { urls: "stun:stun1.l.google.com:3478" },
+    { urls: "stun:stun1.l.google.com:5349" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:5349" },
+    { urls: "stun:stun3.l.google.com:3478" },
+    { urls: "stun:stun3.l.google.com:5349" },
+    { urls: "stun:stun4.l.google.com:19302" },
+    { urls: "stun:stun4.l.google.com:5349" },
   ],
 };
 
