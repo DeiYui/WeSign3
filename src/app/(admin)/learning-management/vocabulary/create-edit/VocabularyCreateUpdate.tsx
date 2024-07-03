ef2691c48b1c25217db501dc24f93794521642fb
truongnv <truongnv@vnpt-technology.vn>
2024-07-03 23:32:42 +0700

    Fix bug chỉnh câu hỏi kiểm tra

--- src/app/(admin)/learning-management/vocabulary/create-edit/VocabularyCreateUpdate.tsx
+++ src/app/(admin)/learning-management/vocabulary/create-edit/VocabularyCreateUpdate.tsx
@@ -86,7 +86,7 @@ const VocabularyCreateUpdate: React.FC = () => {
     open: false,
     file: "",
   });
-
+  const [isLoadingUploadLst, setIsLoadingUploadLst] = useState<boolean>(false);
   const [isLoadingFile, setIsLoadingFile] = useState<boolean>(false);
 
   // Loại từ vựng
@@ -221,6 +221,7 @@ const VocabularyCreateUpdate: React.FC = () => {
   };
 
   const handleUpload = async (value: any) => {
+    setIsLoadingUploadLst(true);
     // Tạo FormData để truyền danh sách file cho API upload
     const formData = new FormData();
     fileList.forEach((file: any) => {
@@ -275,95 +276,51 @@ const VocabularyCreateUpdate: React.FC = () => {
     } else {
       message.error("Lỗi tải file");
     }
+    setIsLoadingUploadLst(false);
   };
 
   return (
-    <div className="w-full p-4">
-      <Breadcrumb
-        pageName="Thêm mới từ vựng"
-        itemBreadcrumb={[
-          { pathName: "/", name: "Trang chủ" },
-          {
-            pathName:
-              isPrivate === "true"
-                ? "/learning-management/vocabulary/private"
-                : "/learning-management/vocabulary/public",
-            name: "Danh sách từ vựng",
-          },
-          { pathName: "#", name: "Thêm mới từ vựng" },
-        ]}
-      />
-      <div className="w-full bg-white">
-        <Tabs
-          className="px-4"
-          activeKey={tabKey}
-          onChange={(key) => setTabKey(key)}
-        >
-          <TabPane tab="Thêm một" key="1">
-            <Form
-              form={form}
-              layout="vertical"
-              className="px-4 pb-4"
-              onFinish={(value) => {
-                mutationCreate.mutate({
-                  ...value,
-                  vocabularyImageReqs: value?.vocabularyImageReqs || undefined,
-                  vocabularyVideoReqs: value?.vocabularyVideoReqs || undefined,
-                  private: isPrivate,
-                });
-              }}
-            >
-              <Form.Item
-                name="topicId"
-                label="Chủ đề liên quan"
-                required
-                rules={[
-                  validateRequireInput("Chủ đề liên quan không được bỏ trống"),
-                ]}
-                className="mb-2"
+    <Spin spinning={isLoadingUploadLst}>
+      <div className="w-full p-4">
+        <Breadcrumb
+          pageName="Thêm mới từ vựng"
+          itemBreadcrumb={[
+            { pathName: "/", name: "Trang chủ" },
+            {
+              pathName:
+                isPrivate === "true"
+                  ? "/learning-management/vocabulary/private"
+                  : "/learning-management/vocabulary/public",
+              name: "Danh sách từ vựng",
+            },
+            { pathName: "#", name: "Thêm mới từ vựng" },
+          ]}
+        />
+        <div className="w-full bg-white">
+          <Tabs
+            className="px-4"
+            activeKey={tabKey}
+            onChange={(key) => setTabKey(key)}
+          >
+            <TabPane tab="Thêm một" key="1">
+              <Form
+                form={form}
+                layout="vertical"
+                className="px-4 pb-4"
+                onFinish={(value) => {
+                  mutationCreate.mutate({
+                    ...value,
+                    vocabularyImageReqs:
+                      value?.vocabularyImageReqs || undefined,
+                    vocabularyVideoReqs:
+                      value?.vocabularyVideoReqs || undefined,
+                    private: isPrivate,
+                  });
+                }}
               >
-                <Select
-                  size="large"
-                  className="w-full"
-                  allowClear
-                  placeholder="Chọn chủ đề"
-                  options={allTopics}
-                />
-              </Form.Item>
-              <Form.Item
-                name="vocabularyType"
-                label="Loại từ vựng"
-                required
-                rules={[
-                  validateRequireInput("Loại từ vựng không được bỏ trống"),
-                ]}
-                className="mb-2"
-              >
-                <Select
-                  size="large"
-                  className="w-full"
-                  allowClear
-                  placeholder="Chọn loại từ vựng"
-                  options={[
-                    {
-                      label: "Từ",
-                      value: "WORD",
-                    },
-                    {
-                      label: "Câu",
-                      value: "SENTENCE",
-                    },
-                    {
-                      label: "Đoạn",
-                      value: "PARAGRAPH",
-                    },
-                  ]}
-                />
-              </Form.Item>
-              {vocabularyType ? (
                 <Form.Item
-                  name="content"
-                  label="Ngôn ngữ văn bản"
+                  name="topicId"
+                  label="Chủ đề liên quan"
                   required
                   rules={[
                     validateRequireInput(
@@ -372,176 +329,233 @@ const VocabularyCreateUpdate: React.FC = () => {
                   ]}
                   className="mb-2"
                 >
+                  <Select
+                    size="large"
+                    className="w-full"
+                    allowClear
+                    placeholder="Chọn chủ đề"
+                    options={allTopics}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="vocabularyType"
+                  label="Loại từ vựng"
+                  required
+                  rules={[
+                    validateRequireInput("Loại từ vựng không được bỏ trống"),
+                  ]}
+                  className="mb-2"
+                >
+                  <Select
+                    size="large"
+                    className="w-full"
+                    allowClear
+                    placeholder="Chọn loại từ vựng"
+                    options={[
+                      {
+                        label: "Từ",
+                        value: "WORD",
+                      },
+                      {
+                        label: "Câu",
+                        value: "SENTENCE",
+                      },
+                      {
+                        label: "Đoạn",
+                        value: "PARAGRAPH",
+                      },
+                    ]}
+                  />
+                </Form.Item>
+                {vocabularyType ? (
+                  <Form.Item
+                    name="content"
+                    label="Ngôn ngữ văn bản"
+                    required
+                    rules={[
+                      validateRequireInput(
+                        "Chủ đề liên quan không được bỏ trống",
+                      ),
+                    ]}
+                    className="mb-2"
+                  >
+                    <TextArea
+                      maxLength={200}
+                      showCount
+                      placeholder="Nhập từ vựng"
+                    />
+                  </Form.Item>
+                ) : null}
+
+                <Form.Item name="note" label="Mô tả">
                   <TextArea
                     maxLength={200}
                     showCount
-                    placeholder="Nhập từ vựng"
+                    placeholder="Nhập mô tả"
                   />
                 </Form.Item>
-              ) : null}
+                <Form.Item name="vocabularyType" hidden />
+                <div className="flex flex-col gap-4">
+                  <Form.Item name="vocabularyImageReqs" noStyle />
+                  <Form.Item name="vocabularyVideoReqs" noStyle />
 
-              <Form.Item name="note" label="Mô tả">
-                <TextArea maxLength={200} showCount placeholder="Nhập mô tả" />
-              </Form.Item>
-              <Form.Item name="vocabularyType" hidden />
-              <div className="flex flex-col gap-4">
-                <Form.Item name="vocabularyImageReqs" noStyle />
-                <Form.Item name="vocabularyVideoReqs" noStyle />
-
-                <Upload {...props} showUploadList={false} accept="image/*">
-                  <Button icon={<UploadOutlined />}>Tải file ảnh</Button>
-                </Upload>
-                <Upload {...props} showUploadList={false} accept="video/*">
-                  <Button icon={<UploadOutlined />}>Tải file video</Button>
-                </Upload>
-              </div>
-              <div className="mb-3 flex items-center justify-center gap-4">
-                {preview.fileImage ? (
-                  <Image
-                    className=""
-                    src={preview.fileImage}
-                    alt="Ảnh chủ đề"
-                    style={{ width: 300 }}
-                  />
-                ) : null}
-                {preview.fileVideo ? (
-                  <video controls style={{ width: 400, height: "auto" }}>
-                    <source src={preview.fileVideo} />
-                  </video>
-                ) : null}
-              </div>
-              <div className="flex w-full items-center justify-center gap-4">
-                <Button onClick={() => router.back()}>Huỷ</Button>
-                <Button type="primary" htmlType="submit">
-                  Tạo
-                </Button>
-              </div>
-            </Form>
-          </TabPane>
-
-          <TabPane tab="Thêm nhiều" key="2">
-            <Form
-              form={formUpload}
-              layout="vertical"
-              className="px-4 pb-4"
-              onFinish={(value) => {
-                handleUpload(value);
-              }}
-            >
-              <Form.Item
-                name="topicId"
-                label="Chủ đề liên quan"
-                required
-                rules={[
-                  validateRequireInput("Chủ đề liên quan không được bỏ trống"),
-                ]}
-                className="mb-2"
-              >
-                <Select
-                  size="large"
-                  className="w-full"
-                  allowClear
-                  placeholder="Chọn chủ đề"
-                  options={allTopics}
-                />
-              </Form.Item>
-              <Form.Item
-                name="vocabularyType"
-                label="Loại từ vựng"
-                required
-                rules={[
-                  validateRequireInput("Loại từ vựng không được bỏ trống"),
-                ]}
-                className="mb-2"
-              >
-                <Select
-                  size="large"
-                  className="w-full"
-                  allowClear
-                  placeholder="Chọn loại từ vựng"
-                  options={[
-                    {
-                      label: "Từ",
-                      value: "WORD",
-                    },
-                    {
-                      label: "Câu",
-                      value: "SENTENCE",
-                    },
-                    {
-                      label: "Đoạn",
-                      value: "PARAGRAPH",
-                    },
-                  ]}
-                />
-              </Form.Item>
-              <div className="max-h-[600px] overflow-y-scroll">
-                <CustomUpload
-                  listType="text"
-                  fileList={fileList}
-                  onPreview={handlePreview}
-                  onChange={handleChange}
-                  multiple
-                  customRequest={async ({ file }: any) => {
-                    const isImageOrVideo =
-                      file.type.startsWith("image/") ||
-                      file.type.startsWith("video/");
-                    if (!isImageOrVideo) {
-                      message.error("Chỉ được chọn file video hoặc ảnh.");
-                    }
-                  }}
-                  accept="image/*,video/*"
-                >
-                  <Button loading={isLoadingFile} icon={<UploadOutlined />}>
-                    Chọn File
-                  </Button>
-                </CustomUpload>
-              </div>
-              <div className="flex w-full items-center justify-center gap-4">
-                <Button onClick={() => router.back()}>Huỷ</Button>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  disabled={isLoadingFile}
-                >
-                  Tạo
-                </Button>
-              </div>
-            </Form>
-          </TabPane>
-        </Tabs>
-      </div>
-
-      {/* Modal */}
-      {/* Modal xem trước */}
-      <Modal
-        open={previewFile.open}
-        onCancel={() => setPreviewFile({ open: false, file: "" })}
-        footer={null}
-        width={600}
-        closeIcon={null}
-      >
-        <div className="flex w-full items-center justify-center">
-          {previewFile && (
-            <>
-              {previewFile.file?.originFileObj?.type?.startsWith("image/") ? (
-                <Image
-                  className="w-full"
-                  alt=""
-                  src={previewFile.file.preview}
-                />
-              ) : (
-                <div className="w-full">
-                  <video controls style={{ width: "100%", height: "auto" }}>
-                    <source src={previewFile.file.preview} />
-                  </video>
+                  <Upload {...props} showUploadList={false} accept="image/*">
+                    <Button icon={<UploadOutlined />}>Tải file ảnh</Button>
+                  </Upload>
+                  <Upload {...props} showUploadList={false} accept="video/*">
+                    <Button icon={<UploadOutlined />}>Tải file video</Button>
+                  </Upload>
                 </div>
-              )}
-            </>
-          )}
+                <div className="mb-3 flex items-center justify-center gap-4">
+                  {preview.fileImage ? (
+                    <Image
+                      className=""
+                      src={preview.fileImage}
+                      alt="Ảnh chủ đề"
+                      style={{ width: 300 }}
+                    />
+                  ) : null}
+                  {preview.fileVideo ? (
+                    <video controls style={{ width: 400, height: "auto" }}>
+                      <source src={preview.fileVideo} />
+                    </video>
+                  ) : null}
+                </div>
+                <div className="flex w-full items-center justify-center gap-4">
+                  <Button onClick={() => router.back()}>Huỷ</Button>
+                  <Button type="primary" htmlType="submit">
+                    Tạo
+                  </Button>
+                </div>
+              </Form>
+            </TabPane>
+
+            <TabPane tab="Thêm nhiều" key="2">
+              <Form
+                form={formUpload}
+                layout="vertical"
+                className="px-4 pb-4"
+                onFinish={(value) => {
+                  handleUpload(value);
+                }}
+              >
+                <Form.Item
+                  name="topicId"
+                  label="Chủ đề liên quan"
+                  required
+                  rules={[
+                    validateRequireInput(
+                      "Chủ đề liên quan không được bỏ trống",
+                    ),
+                  ]}
+                  className="mb-2"
+                >
+                  <Select
+                    size="large"
+                    className="w-full"
+                    allowClear
+                    placeholder="Chọn chủ đề"
+                    options={allTopics}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="vocabularyType"
+                  label="Loại từ vựng"
+                  required
+                  rules={[
+                    validateRequireInput("Loại từ vựng không được bỏ trống"),
+                  ]}
+                  className="mb-2"
+                >
+                  <Select
+                    size="large"
+                    className="w-full"
+                    allowClear
+                    placeholder="Chọn loại từ vựng"
+                    options={[
+                      {
+                        label: "Từ",
+                        value: "WORD",
+                      },
+                      {
+                        label: "Câu",
+                        value: "SENTENCE",
+                      },
+                      {
+                        label: "Đoạn",
+                        value: "PARAGRAPH",
+                      },
+                    ]}
+                  />
+                </Form.Item>
+                <div className="max-h-[600px] overflow-y-scroll">
+                  <CustomUpload
+                    listType="text"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                    multiple
+                    customRequest={async ({ file }: any) => {
+                      const isImageOrVideo =
+                        file.type.startsWith("image/") ||
+                        file.type.startsWith("video/");
+                      if (!isImageOrVideo) {
+                        message.error("Chỉ được chọn file video hoặc ảnh.");
+                      }
+                    }}
+                    accept="image/*,video/*"
+                  >
+                    <Button loading={isLoadingFile} icon={<UploadOutlined />}>
+                      Chọn File
+                    </Button>
+                  </CustomUpload>
+                </div>
+                <div className="flex w-full items-center justify-center gap-4">
+                  <Button onClick={() => router.back()}>Huỷ</Button>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    disabled={isLoadingFile}
+                  >
+                    Tạo
+                  </Button>
+                </div>
+              </Form>
+            </TabPane>
+          </Tabs>
         </div>
-      </Modal>
-    </div>
+
+        {/* Modal */}
+        {/* Modal xem trước */}
+        <Modal
+          open={previewFile.open}
+          onCancel={() => setPreviewFile({ open: false, file: "" })}
+          footer={null}
+          width={600}
+          closeIcon={null}
+        >
+          <div className="flex w-full items-center justify-center">
+            {previewFile && (
+              <>
+                {previewFile.file?.originFileObj?.type?.startsWith("image/") ? (
+                  <Image
+                    className="w-full"
+                    alt=""
+                    src={previewFile.file.preview}
+                  />
+                ) : (
+                  <div className="w-full">
+                    <video controls style={{ width: "100%", height: "auto" }}>
+                      <source src={previewFile.file.preview} />
+                    </video>
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        </Modal>
+      </div>
+    </Spin>
   );
 };
 
